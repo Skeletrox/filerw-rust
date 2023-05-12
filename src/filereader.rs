062e1279b9181435111fda7322723d598268d04e
Skeletrox <android.svr@gmail.com>
2023-05-11 17:51:52 -0700

    Enable multiline file read functionality

--- src/filereader.rs
+++ src/filereader.rs
@@ -32,15 +32,31 @@ pub mod simplereader {
         }
         return returnable;
     }
+
     // Explicitly define publicity for each child module/member.
     pub fn file_name_size(path: &String) -> usize {
         path.len()
     }
+
     // Read file and get a vector of values
     pub fn read_numbers_from_file(path: &String) -> VecDeque<i32> {
         let csv_contents = read_file(path);
         let returned_values = csv_numbers_to_vector(csv_contents);
         return returned_values;
+    }
+
+    // Read multiple lines from a file to return multiple vectors of values
+    pub fn read_multiple_lines_from_file(path: &String) -> Vec<VecDeque<i32>> {
+        let file_contents = read_file(path);
+        let lines = file_contents.split("\n");
+        let mut returnable = Vec::new();
+        for line in lines {
+            if line.len() > 0 {
+                let current_line_vector = csv_numbers_to_vector(line.to_string());
+                returnable.push(current_line_vector);
+            }
+        }
+        return returnable;
     }
 
     #[cfg(test)]
@@ -64,6 +80,41 @@ pub mod simplereader {
             for i in 0..ground_truth.len() {
                 assert_eq!(ground_truth.get(i), res.get(i));
             }
+        }
+
+        #[test]
+        fn test_read_multiple_vals() {
+            use crate::filewriter::simplewriter::append_str_to_file;
+            use rand::Rng;
+            let num_lines = 5;
+            let num_vals = 10;
+            let file_name = "/tmp/rmv_test.tst".to_string();
+            let mut rng = rand::thread_rng();
+            let mut writing_values: Vec<VecDeque<i32>> = Vec::<VecDeque::<i32>>::new();
+            for _ in 0..num_lines {
+                let mut values: VecDeque<i32> = VecDeque::<i32>::new();
+                for _ in 0..num_vals {
+                    values.push_back(rng.gen_range(0..65536));
+                }
+                let mut writable_string = values
+                    .iter()
+                    .map(|i| i.to_string())
+                    .collect::<Vec<String>>().join(",");
+                writable_string.push('\n');
+                append_str_to_file(&file_name, &writable_string);
+                writing_values.push(values);
+            }
+            let read_values = read_multiple_lines_from_file(&file_name);
+            assert_eq!(writing_values.len(), read_values.len());
+            for i in 0..writing_values.len() {
+                let curr_line_wr = &writing_values[i];
+                let curr_line_re = &read_values[i];
+                assert_eq!((*curr_line_wr).len(), (*curr_line_re).len());
+                for j in 0..curr_line_wr.len() {
+                    assert_eq!(curr_line_wr[j], curr_line_re[j]);
+                }
+            }
+
         }
     }
 }
